f284a1b0840775d1c6c64748a94164940f336baa
LZJun <zhanjunlee123@gmail.com>
2025-03-27 16:22:31 +0800

    removal of add to cart mock test

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,8 +8,7 @@ use App\Models\Cart;
 class CartController extends Controller
 {
     function addToCart(Request $req){
-        // $userId = session('user_id');
-        $userId = 1;
+        $userId = session('user_id');
         if (!$userId) {
             return redirect('login');
         }
@@ -31,8 +30,7 @@ class CartController extends Controller
             // Create new cart item if product doesn't exist
             Cart::create([
                 'product_id' => $req->product_id,
-                // 'user_id' => $req->user_id,
-                'user_id' => $userId,
+                'user_id' => $req->user_id,
                 'qty' => $req->qty,
             ]);
             $message = 'Product added to cart successfully!';
